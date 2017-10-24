8f85a6b1c704858d340275252f567e9ea09b0fc8
Marián Hlaváč <hlavac95@gmail.com>
2017-10-25 00:04:22 +0200

    Prepare the packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='noilibrarian',
+      version='0.1',
+      description='A small utility for sorting samples used by NOI Manager',
+      url='https://github.com/noi-synth/noi-librarian',
+      author='Marian Hlavac',
+      author_email='mail@majko.cz',
+      license='',
+      packages=['noilibrarian'],
+      zip_safe=False)
